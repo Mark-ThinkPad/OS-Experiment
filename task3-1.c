99c1886b4e8da3cadd747413924aa3c79b6a9b50
Mark-ThinkPad <1372469698@qq.com>
2020-05-01 17:23:57 +0800

    play somthing new

--- task3-1.c
+++ task3-1.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int pid = 0;
+    pid = fork();
+    switch (pid)
+    {
+    case -1:
+        printf("fork fail!\n");
+        exit(1);
+    case 0:
+        execl("/bin/ls", "ls", "-l", "-color", NULL);
+        printf("exec fail!\n");
+        exit(1);
+    default:
+        wait(NULL);
+        printf("ls completed!\n");
+        exit(0);
+    }
+
     return 0;
 }
