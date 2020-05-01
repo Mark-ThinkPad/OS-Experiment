fa7565699cd2772e29e13b9df04dd408886ad530
Mark-ThinkPad <1372469698@qq.com>
2020-05-01 21:56:58 +0800

    play somthing new

--- task3-2.c
+++ task3-2.c
@@ -3,6 +3,34 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int p1=0, p2=0, i=0;
+    while ((p1=fork()) == -1);
+    if (p1 == 0)
+    {
+        lockf(1, 1, 0);
+        for (i = 0; i < 10; i++)
+            printf("daughter %d\n", i);
+        lockf(1, 0, 0);
+    }
+    else
+    {
+        while ((p2=fork()) == -1);
+        if (p2 == 0)
+        {
+            lockf(1, 1, 0);
+            for (i = 0; i < 10; i++)
+                printf("son %d\n", i);
+            lockf(1, 0, 0);
+        }
+        else
+        {
+            lockf(1, 1, 0);
+            for (i = 0; i < 10; i++)
+                printf("parent %d\n", i);
+            lockf(1, 0, 0);
+        }
+        
+    }
+    
     return 0;
 }
