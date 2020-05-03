cd182a3ab264a99a5081df1ea732aaecda9941fc
Mark-ThinkPad <1372469698@qq.com>
2020-05-03 15:40:48 +0800

    play somthing new

--- task5.c
+++ task5.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int pid1=0, pid2=0;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int fd[2];
+    char outpipe[100], inpipe[100];
+    pipe(fd);
+    while ((pid1=fork()) == -1);
+    if (pid1 == 0)
+    {
+        lockf(fd[1], 1, 0);
+        sprintf(outpipe, "child 1 process is sending message!");
+        write(fd[1], outpipe, 50);
+        sleep(5);
+        lockf(fd[1], 0, 0);
+        exit(0);
+    }
+    else
+    {
+        while ((pid2=fork()) == -1);
+        if (pid2 == 0)
+        {
+            lockf(fd[1], 1, 0);
+            sprintf(outpipe, "child 2 process is sending message!");
+            write(fd[1], outpipe, 50);
+            sleep(5);
+            lockf(fd[1], 0, 0);
+            exit(0);
+        }
+        else
+        {
+            wait(0);
+            read(fd[0], inpipe, 50);
+            printf("%s\n", inpipe);
+            wait(0);
+            read(fd[0], inpipe, 50);
+            printf("%s\n", inpipe);
+            exit(0);
+        }   
+    }
+
     return 0;
 }
